57dcfb11a988df9ca5f160e085bb5907d6920680
Zack <111192745+zackinfradev@users.noreply.github.com>
2024-01-05 11:10:55 -0700

    Rewrite install_neovim() to prefer Appimage installation

--- nvim-basic-install.sh
+++ nvim-basic-install.sh
@@ -1,88 +1,62 @@
 #!/bin/bash
 
-success_via_package(){
-    echo "Neovim successfully installed via package manager."
-}
+set -e
 
 success_via_appimage(){
     echo "Neovim successfully installed via appimage."
 }
 
+success_via_package(){
+    echo "Neovim successfully installed via package."
+}
+
 install_failed(){
-    echo "Neovim light install failed!"
-    exit 1
+    echo "Neovim failed to install!"
 }
 
 install_neovim(){
+    # Try installing via AppImage first
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    ./nvim.appimage --appimage-extract &> /dev/null
+    sudo mv squashfs-root / &> /dev/null
+    sudo ln -s /squashfs-root/AppRun /usr/bin/nvim &> /dev/null
+
+    if command -v nvim &> /dev/null; then
+        success_via_appimage
+        return
+    else
+        echo "AppImage installation failed, attempting package manager installation."
+    fi
+
+    # On fail resort to package manager
+
     if command -v apt-get &> /dev/null; then
         sudo apt-get install neovim
-        if [ $? -eq 0 ]; then
-            success_via_package
-        else
-            install_failed
-        fi
     elif command -v yum &> /dev/null; then
         sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
         sudo yum install -y neovim python3-neovim
-        if [ $? -eq 0 ]; then
-            success_via_package
-        else
-            install_failed
-        fi
     elif command -v pacman &> /dev/null; then
         sudo pacman -S neovim
-        if [ $? -eq 0 ]; then
-            success_via_package
-        else
-            install_failed
-        fi
     elif command -v dnf &> /dev/null; then
         sudo dnf install -y neovim python3-neovim
-        if [ $? -eq 0 ]; then
-            success_via_package
-        else
-            install_failed
-        fi
     else
-        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-        chmod u+x nvim.appimage
-        if ! command -v ./nvim.appimage; then
-            ./nvim.appimage --appimage-extract
-            if ! command -v ./squashfs-root/AppRun --version; then
-                # Optional: exposing nvim globally.
-                sudo mv squashfs-root /
-                sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-                command -v /usr/bin/nvim &> /dev/null
-                if [ $? -eq 0 ]; then
-                    success_via_appimage
-                else
-                    install_failed
-                fi
-            else
-                success_via_appimage
-            fi
-        else
-            success_via_appimage
-        fi
+        install_failed
+        return
     fi
-}
 
-copy_items(){
-    cp -r ./nvim-basic-installer/.config ~/.config
-    if [ $? -ne 0 ]; then
-        exit $?
+    if command -v nvim &> /dev/null; then
+        success_via_package
+    else
+        install_failed
     fi
-}
-
-find_init_lua(){
-    remap_lines=$(wc -l < ~/.config/nvim/init.lua)
-    echo "Moved $remap_lines config lines into init.lua"
 }
 
 install_neovim
 
-copy_items
+cp -r ./.config ~/.config
 
-find_init_lua
+remap_lines=$(wc -l < ~/.config/nvim/init.lua)
+echo "Moved $remap_lines config lines into init.lua"
 
 exit 0
